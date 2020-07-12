9c633af5f0ba015da4342014e5afd73e05821d35
julian1 <git@julian1.io>
2020-07-12 15:34:43 +1000

    ok seems to be toggling one pin/direction

---
+++ examples/stm32/f4/stm32f4-discovery/whoot/whoot.c
@@ -0,0 +1,110 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+static void gpio_setup(void)
+{
+	/* Enable GPIOD clock. */
+	/* Manually: */
+	// RCC_AHB1ENR |= RCC_AHB1ENR_IOPDEN;
+	/* Using API functions: */
+	// rcc_periph_clock_enable(RCC_GPIOD);
+	rcc_periph_clock_enable(RCC_GPIOE); // JA
+
+	rcc_periph_clock_enable(RCC_GPIOD); // JA
+
+	/* Set GPIO12 (in GPIO port D) to 'output push-pull'. */
+	/* Manually: */
+	// GPIOD_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2));
+	// GPIOD_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4));
+	/* Using API functions: */
+	// gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+
+
+   // gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_OTYPE_OD, GPIO0); // JA
+   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0); // JA
+
+
+   gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0); // JA
+   gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1); // JA
+   gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO2); // JA
+
+/*
+#define  GPIO_PUPD_NONE   0x0
+#define  GPIO_PUPD_PULLUP   0x1
+#define  GPIO_PUPD_PULLDOWN   0x2
+*/
+
+
+}
+
+int main(void)
+{
+	int i;
+
+	gpio_setup();
+
+  // gpio_toggle(GPIOD, GPIO0);  // JA enable
+  gpio_set(GPIOD, GPIO0);  
+
+
+    gpio_set(GPIOD, GPIO1);  
+    gpio_clear(GPIOD, GPIO2);  
+
+	/* Blink the LED (PC8) on the board. */
+	while (1) {
+		/* Manually: */
+		// GPIOD_BSRR = GPIO12;		/* LED off */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+		// GPIOD_BRR = GPIO12;		/* LED on */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+
+		/* Using API functions gpio_set()/gpio_clear(): */
+		// gpio_set(GPIOD, GPIO12);	/* LED off */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+		// gpio_clear(GPIOD, GPIO12);	/* LED on */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+
+		/* Using API function gpio_toggle(): */
+
+
+		// gpio_toggle(GPIOD, GPIO12);	/* LED on/off */
+
+    gpio_toggle(GPIOE, GPIO2);  // JA
+
+    gpio_toggle(GPIOD, GPIO1);  // JA
+    gpio_toggle(GPIOD, GPIO2);  // JA
+
+
+
+
+    for (i = 0; i < 1000000; i++) { /* Wait a bit. */
+			__asm__("nop");
+		}
+	}
+
+	return 0;
+}
