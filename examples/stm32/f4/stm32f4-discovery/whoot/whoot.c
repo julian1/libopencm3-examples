ca0ae35291bd0a16f4440d6c512976e28f53d005
julian1 <git@julian1.io>
2020-07-12 20:10:52 +1000

    two speed

--- examples/stm32/f4/stm32f4-discovery/whoot/whoot.c
+++ examples/stm32/f4/stm32f4-discovery/whoot/whoot.c
@@ -49,10 +49,10 @@ static void gpio_setup(void)
 }
 
 // think coil has 4 power positions.
-// +5,0, +5,+5, 0,+5, 0,0, 
+// +5,0, +5,+5, 0,+5, 0,0,
 // but we won't use them all.
 // and we have to get the polarity right...
-/* 
+/*
 static int ia[][4] = {
   { 1,0,   0, 1 },
   { 1,1,   0, 0 },
@@ -69,9 +69,9 @@ static int ia[][4] = {
 // http://rfanat.ru/stanki_chpu/BIPOLAR_Stepper_Motor_Driver_Circuits_74194.html
 
 /*
-  3.75V 
+  3.75V
     turning quite fast 1 rps. uses 0.16A
-    1 revolution every 10 sec uses 1.4 amps. 
+    1 revolution every 10 sec uses 1.4 amps.
     runnning a click every second uses 1.69 amps. which is a lot.
 
     eg. when slow - its almost an open short.
@@ -79,15 +79,16 @@ static int ia[][4] = {
 
 int main(void)
 {
-	int i = 0;
+	uint32_t i = 0;
 	int j = 0;
+
 
 	gpio_setup();
 
-  // this needs to be set/high - to get the voltage switch 
+  // this needs to be set/high - to get the voltage switch
   // alternatively we pulse this ... to reduce current...
   gpio_set(GPIOD, GPIO0);   // JA enable A
-  gpio_set(GPIOD, GPIO5);   // enable B 
+  gpio_set(GPIOD, GPIO5);   // enable B
 
 
   // full wave drive
@@ -98,33 +99,59 @@ int main(void)
     switch(i % 4) {
 
       // NO... the pins are around the wrong way.
-      // the wave form pins - are different. for particular pair should never be both set or both clear. 
+      // the wave form pins - are different. for particular pair should never be both set or both clear.
 
-      case 0: 
-        // gpio_set(  GPIOD, GPIO1);  gpio_clear(GPIOD, GPIO2);  gpio_set(GPIOD, GPIO3);  gpio_clear(GPIOD, GPIO4);  
-        gpio_set(GPIOD, GPIO1 | GPIO3);  gpio_clear(GPIOD, GPIO2 | GPIO4);  
+      case 0:
+        // gpio_set(  GPIOD, GPIO1);  gpio_clear(GPIOD, GPIO2);  gpio_set(GPIOD, GPIO3);  gpio_clear(GPIOD, GPIO4);
+        gpio_set(GPIOD, GPIO1 | GPIO3); gpio_clear(GPIOD, GPIO2 | GPIO4);
         break;
-      case 1: 
-        // gpio_set(  GPIOD, GPIO1);  gpio_clear(GPIOD, GPIO2);  gpio_clear(GPIOD, GPIO3);  gpio_set(GPIOD, GPIO4);  
-        gpio_set(GPIOD, GPIO1 | GPIO4);  gpio_clear(GPIOD, GPIO2 | GPIO3);  
+      case 1:
+        // gpio_set(  GPIOD, GPIO1);  gpio_clear(GPIOD, GPIO2);  gpio_clear(GPIOD, GPIO3);  gpio_set(GPIOD, GPIO4);
+        gpio_set(GPIOD, GPIO1 | GPIO4); gpio_clear(GPIOD, GPIO2 | GPIO3);
         break;
-      case 2: 
-        // gpio_clear(  GPIOD, GPIO1);  gpio_set(GPIOD, GPIO2);  gpio_clear(GPIOD, GPIO3);  gpio_set(GPIOD, GPIO4);  
-        gpio_set(GPIOD, GPIO2 | GPIO4);  gpio_clear(GPIOD, GPIO1 | GPIO3);  
+      case 2:
+        // gpio_clear(  GPIOD, GPIO1);  gpio_set(GPIOD, GPIO2);  gpio_clear(GPIOD, GPIO3);  gpio_set(GPIOD, GPIO4);
+        gpio_set(GPIOD, GPIO2 | GPIO4); gpio_clear(GPIOD, GPIO1 | GPIO3);
         break;
-      case 3: 
-        // gpio_clear(  GPIOD, GPIO1);  gpio_set(GPIOD, GPIO2);  gpio_set(GPIOD, GPIO3);  gpio_clear(GPIOD, GPIO4);  
-        gpio_set(GPIOD, GPIO2 | GPIO3);    gpio_clear(GPIOD, GPIO1 | GPIO4);  
+      case 3:
+        // gpio_clear(  GPIOD, GPIO1);  gpio_set(GPIOD, GPIO2);  gpio_set(GPIOD, GPIO3);  gpio_clear(GPIOD, GPIO4);
+        gpio_set(GPIOD, GPIO2 | GPIO3); gpio_clear(GPIOD, GPIO1 | GPIO4);
         break;
-    } 
+    }
 
-    gpio_toggle(GPIOE, GPIO0);  // led blink 
 
+
+    switch((i / 200) % 2 ) {
+
+      case 0:  {
+        gpio_clear(GPIOE, GPIO0);
+
+        for (j = 0; j < 10000; j++)
+          __asm__("nop");
+
+        break;
+      }
+
+      case 1: {
+        gpio_set(GPIOE, GPIO0);
+
+        for (j = 0; j < 30000; j++)
+          __asm__("nop");
+        break;
+      }
+
+    }
+
+    // for (j = 0; j < 3000; j++) { // about the fastest it will go. 10rpm? need to increase the voltage past 10V.
+    // for (j = 0; j < 5000; j++) { //
     // for (j = 0; j < 10000; j++) { /* Wait a bit. */
-    for (j = 0; j < 100000; j++) { /* Wait a bit. */
-    // for (j = 0; j < 2000000; j++) { /* Wait a bit. */
+    // for (j = 0; j < 30000; j++) { /* Wait a bit. */
+    // for (j = 0; j < 100000; j++) { /* Wait a bit. */
+    /*
+    for (j = 0; j < 2000000; j++) {
 			__asm__("nop");
 		}
+    */
 	}
 
 	return 0;
