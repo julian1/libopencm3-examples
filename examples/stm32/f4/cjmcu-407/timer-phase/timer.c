55908765fde40a618761c105d8bd7052f0528559
julian1 <mail@julian1.io>
2020-07-24 21:31:53 +1000

    tidy

--- examples/stm32/f4/cjmcu-407/timer-phase/timer.c
+++ examples/stm32/f4/cjmcu-407/timer-phase/timer.c
@@ -7,7 +7,7 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
 
-// OK. THIS IS EXACTLY THE SAME DAMN CODE. EXCEPT CHANGED FOR TIM4 
+// OK. THIS IS EXACTLY THE SAME DAMN CODE. EXCEPT CHANGED FOR TIM4
 // AF... AF2... not AF1
 
 //	timer_set_oc_polarity_high(TIM4, TIM_OC1);
@@ -16,7 +16,10 @@
 /* Reset repetition counter value. */
 //	timer_set_repetition_counter(TIM1, 0);
 
-
+/*
+  trick here - is that we don't use the arr reload. we just toggle the outputs when they reach
+  the compare value
+*/
 
 int main(void)
 {
@@ -28,25 +31,18 @@ int main(void)
 
 
   gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO12 | GPIO13 | GPIO14 | GPIO15);
-  gpio_set_af(GPIOD, GPIO_AF2, GPIO12 | GPIO13 | GPIO14 | GPIO15); 
+  gpio_set_af(GPIOD, GPIO_AF2, GPIO12 | GPIO13 | GPIO14 | GPIO15);
   gpio_set_output_options(GPIOD, GPIO_OTYPE_PP, GPIO_OSPEED_100MHZ, GPIO12 | GPIO13 | GPIO14 | GPIO15);
 
 
   rcc_periph_reset_pulse(RST_TIM4);   // good practice
-	timer_set_prescaler(TIM4, 65535 ); // JA - blinks 1x/s. eg. consistent with 64MHz, which is documented .
-	// timer_set_prescaler(TIM4, (rcc_apb1_frequency * 2) / 100 );
-	// timer_set_prescaler(TIM4, (rcc_apb2_frequency ) / 10 );   // higher is faster - which makes no sense?
-                                                               // is this overflowing...
-                                                               // it's rougly correct -
+	timer_set_prescaler(TIM4, 65535 ); // JA - blinks 1/s.
+
   // timer_disable_preload(TIM4);
   // timer_continuous_mode(TIM4);
 
-
-
-  // CMS centre
-
   timer_set_mode(TIM4, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-  // timer_set_mode(TIM4, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_CENTER_1, TIM_CR1_DIR_UP); 
+  // timer_set_mode(TIM4, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_CENTER_1, TIM_CR1_DIR_UP);
   timer_enable_preload(TIM4);
   timer_enable_break_main_output(TIM4); // what does this do
   timer_set_period(TIM4, 2000);
@@ -59,7 +55,7 @@ int main(void)
   timer_set_oc_value(TIM4, TIM_OC1, 1000);
 
 
-  timer_set_oc_mode(TIM4, TIM_OC2, TIM_OCM_TOGGLE); // OK. this inverts from PWM1. eg. its the bottom. 
+  timer_set_oc_mode(TIM4, TIM_OC2, TIM_OCM_TOGGLE); // OK. this inverts from PWM1. eg. its the bottom.
   timer_enable_oc_output(TIM4, TIM_OC2);
   timer_set_oc_value(TIM4, TIM_OC2, 1); // 1 not zero, to catch on the upward count...
 
