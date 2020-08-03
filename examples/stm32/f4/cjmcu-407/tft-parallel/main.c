8c9831fbbc49c13432759518551b5839b31ef47c
julian1 <mail@julian1.io>
2020-08-03 22:38:22 +1000

    update

--- examples/stm32/f4/cjmcu-407/tft-parallel/main.c
+++ examples/stm32/f4/cjmcu-407/tft-parallel/main.c
@@ -1,15 +1,27 @@
 /*
   8-bit parallel.
 
+  in 8 bit parallel.
+    is the command still 16 bit.
+    which byte is loaded first.
 
-  - general 
+  - general
     https://controllerstech.com/interface-tft-display-with-stm32/
 
   https://www.displayfuture.com/Display/datasheet/controller/ILI9341.pdf
 
-  So we would have to bit-bash.  
+  simple,
+  https://github.com/adafruit/Adafruit_ILI9341
+  
+  primitives
+    https://github.com/adafruit/Adafruit-GFX-Library/blob/master/Adafruit_SPITFT.cpp
+
+  optimised. 70 forks. but uses spi.
+    https://github.com/PaulStoffregen/ILI9341_t3
+
+  So we would have to bit-bash.
     we can use the millisec msleep to begin with.
-    a nop loop for micro-second. just write the inner loop - execute it a thousand times and blink led - 
+    a nop loop for micro-second. just write the inner loop - execute it a thousand times and blink led -
 
   Note - rather than setting gpio individually - would have to left/shift values into place for the bus.
     but would likely be pretty damn fast.
@@ -25,18 +37,12 @@
 
 #include "clock.h"
 
-// GPIOB
-#define LCD_NSS   GPIO12 // not AF
-#define LCD_SCK   GPIO13
-#define LCD_MISO  GPIO14 // not used
-#define LCD_MOSI  GPIO15
 
 // GPIOE
 #define LCD_WR    GPIO11
 #define LCD_RS    GPIO12
 #define LCD_RST   GPIO13
 
-#define LCD_SPI   SPI2
 
 static void led_setup(void)
 {
@@ -44,22 +50,115 @@ static void led_setup(void)
   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0); // JA
 }
 
+/*
+  gpio_common_all.c
 
-// should put control registers on different port - this would allow 16 bit parallel bit bashing. if we wanted 
+void gpio_set(uint32_t gpioport, uint16_t gpios)
+{
+	GPIO_BSRR(gpioport) = gpios;
+}
+
+void  gpio_clear(uint32_t gpioport, uint16_t gpios)
+{
+	GPIO_BSRR(gpioport) = (gpios << 16);
+}
+
+void gpio_port_write(uint32_t gpioport, uint16_t data)
+{
+	GPIO_ODR(gpioport) = data;
+}
+*/
+
+// should put control registers on different port - this would allow 16 bit parallel bit bashing
+// without hardly any code changes.
+
+#if 0
+
+void Adafruit_SPITFT::sendCommand16(uint16_t commandWord,
+                                    const uint8_t *dataBytes,
+                                    uint8_t numDataBytes) {
+  SPI_BEGIN_TRANSACTION();
+  if (_cs >= 0)
+    SPI_CS_LOW();
+
+  if (numDataBytes == 0) {
+    SPI_DC_LOW();             // Command mode
+    SPI_WRITE16(commandWord); // Send the command word
+    SPI_DC_HIGH();            // Data mode
+  }
+  for (int i = 0; i < numDataBytes; i++) {
+    SPI_DC_LOW();             // Command mode
+    SPI_WRITE16(commandWord); // Send the command word
+    SPI_DC_HIGH();            // Data mode
+    commandWord++;
+    SPI_WRITE16((uint16_t)pgm_read_byte(dataBytes++));
+  }
+
+  if (_cs >= 0)
+    SPI_CS_HIGH();
+  SPI_END_TRANSACTION();
+}
+
+#endif
+
+
+static void sendCommand16(uint16_t commandWord, const uint8_t *dataBytes, uint8_t numDataBytes) {
+/*
+  SPI_BEGIN_TRANSACTION();
+  if (_cs >= 0)
+    SPI_CS_LOW();
+
+  if (numDataBytes == 0) {
+    SPI_DC_LOW();             // Command mode
+    SPI_WRITE16(commandWord); // Send the command word
+    SPI_DC_HIGH();            // Data mode
+  }
+  for (int i = 0; i < numDataBytes; i++) {
+    SPI_DC_LOW();             // Command mode
+    SPI_WRITE16(commandWord); // Send the command word
+    SPI_DC_HIGH();            // Data mode
+    commandWord++;
+    SPI_WRITE16((uint16_t)pgm_read_byte(dataBytes++));
+  }
+
+  if (_cs >= 0)
+    SPI_CS_HIGH();
+  SPI_END_TRANSACTION();
+*/
+}
+
+
 
 int main(void)
 {
   clock_setup();
   led_setup();
 
-  rcc_periph_clock_enable( RCC_GPIOB );
   rcc_periph_clock_enable( RCC_GPIOE );
+  rcc_periph_clock_enable( RCC_GPIOD );
+
+  // #define GPIO12                              (1 << 12)
+
+  gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, 0xff ); // JA first 8 bits.
+
+  // gpio_set does not clear.... it does oring operations...
+  // set everything to 0
+  gpio_clear(GPIOD, 0xff);  // next 8 bits.
+
+  // so to write the register needs a clear first. then set the positive values.
+  // gpio_set is not | in the register it is doing something more....
+  // gpio_set(GPIOD, GPIO5 );  // turn on, referenced to gnd
+  // gpio_set(GPIOD, 0b0000000000000000);  // turn on ????
+  // gpio_set(GPIOD, 1 << 12 );  // turn on ????
 
 
-  // gpio_set( GPIOB, 0b1000000000100000 << 16 );   // should set pa5 and pa15
-  gpio_set( GPIOB, 0b1000000000100000  );   // should set pa5 and pa15, really need to test.
+  gpio_port_write(GPIOD, 1 << 5);   // not sure that atomic, but more useful...
 
-	while (1) {
+ 	while (1) {
+
+    // gpio_toggle(GPIOD, 1 << 5 );  // blink
+
+
     gpio_toggle(GPIOE, GPIO0);  // toggle led
     msleep(300);
 	}
