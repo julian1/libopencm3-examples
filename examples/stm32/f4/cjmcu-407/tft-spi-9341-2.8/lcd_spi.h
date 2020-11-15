516dadd172ba1408c453db0f5449c26faa898bd3
julian1 <git@julian1.io>
2020-11-15 11:35:30 +1100

    tidy

--- examples/stm32/f4/cjmcu-407/tft-spi-9341-2.8/lcd_spi.h
+++ examples/stm32/f4/cjmcu-407/tft-spi-9341-2.8/lcd_spi.h
@@ -1,26 +1,25 @@
 
 
-
+// spi primitives, that hide underlying stm32 spi details.
 
 void lcd_spi_setup( void );
-
 
 void assert_cs(void);
 
 void deassert_rst(void);
 void assert_rst(void);
 
+void turn_on_backlight( void );
+
+
+// only  the following should really be used by gfx
+
 void set_command(void );
 void set_data(void );
 
-
-void turn_on_backlight( void );
-
 void delay( uint16_t x );
 
-
 void send8( uint8_t x );
-
 
 void sendCommand(uint8_t command, const uint8_t *dataBytes, uint8_t numDataBytes);
 void sendCommand0(uint8_t command);
